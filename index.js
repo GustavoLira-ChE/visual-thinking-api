540d4c25f10841c08067409dabd4bec9d22bfee9
Gustavo Lira <gu.lirar23@gmail.com>
2022-05-02 11:37:24 -0500

    chore: added API endpoint to complete requirements

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
-const VisualPartnersController = require('./lib/controller/VisualPartnersController');
-const VisualPartnersService = require('./lib/services/visualPartnersService');
-const Reader = require('./lib/utils/reader');
+const VisualPartnersController = require("./lib/controller/VisualPartnersController");
+const VisualPartnersService = require("./lib/services/visualPartnersService");
+const Reader = require("./lib/utils/reader");
 
-const students = Reader.readJsonFile('./db/visualpartners.json');
+const students = Reader.readJsonFile("./db/visualpartners.json");
 //const email = VisualPartnersController.getVisualPartnersCertificate()
 
 const studentsCertificate = VisualPartnersService.studentsCertificateEmail(students);
